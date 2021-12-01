bfa8d18800869edc579ffc4beb236fb5d93fde62
Ewowers <84778797+Ewowers@users.noreply.github.com>
2021-12-01 18:27:19 +0600

    map 2.0

--- src/component/map.jsx
+++ src/component/map.jsx
@@ -4,12 +4,18 @@ export const Map = () => {
   return (
     <div id="map">
       <Menu />
-      <iframe
+      {/*<iframe
         src="https://yandex.ru/map-widget/v1/?um=constructor%3A87f750877ab9fe5f09bdf546736bd7ec852210df5bdda9b6923c043bb0bb6bb0&amp;source=constructor"
         width={100 + "%"}
         height={100 + "%"}
         frameBorder="0"
-      />
+      />*/}
+      <iframe
+        src="https://yandex.ru/map-widget/v1/?um=constructor%3A4337f23c496ac2946c430e2643c7b65b639ebe30480699698927890eb9b9be5c&amp;source=constructor"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+      ></iframe>
     </div>
   );
 };
