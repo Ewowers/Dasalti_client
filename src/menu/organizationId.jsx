e9b2fa7341d46e63fb956643c4a2f006e09a4ff8
Ewowers <84778797+Ewowers@users.noreply.github.com>
2021-12-04 20:17:29 +0600

    база

--- src/menu/organizationId.jsx
+++ src/menu/organizationId.jsx
@@ -1,9 +1,52 @@
 import axios from "axios";
-
+import { Form, Input, Button, Space, Typography } from "antd";
+import { useEffect, useState } from "react";
 export const OrganizationId = ({ title }) => {
+  const [state, setState] = useState([]);
+  const { Title, Paragraph } = Typography;
   let param = title.split("-");
-  let titles = "";
   param = param[param.length - 1];
+  let titles = params(param);
+  const onFinish = (values) => {
+    console.log(values);
+    axios.post("http://localhost:5000/api/directory/create/" + param, { ...values, category: param }).then((res) => get());
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  const get = () => {
+    axios.get("http://localhost:5000/api/directory/" + param).then((res) => setState(res.data));
+  };
+  useEffect(() => {
+    get();
+  }, []);
+  return (
+    <>
+      <Title level={3} style={{ textAlign: "center" }}>
+        {titles}
+      </Title>
+      <Space direction="vertical">
+        {state.map((item, i) => {
+          return <Paragraph key={i}>{item.title}</Paragraph>;
+        })}
+      </Space>
+      <Form onFinish={onFinish} onFinishFailed={onFinishFailed}>
+        <div style={{ display: "flex" }}>
+          <Form.Item name="title" style={{ width: "100%" }}>
+            <Input size="small" style={{ width: "100%" }} />
+          </Form.Item>
+          <Form.Item>
+            <Button size="small" htmlType="submit" style={{ background: "#cec7eb" }}>
+              Создать
+            </Button>
+          </Form.Item>
+        </div>
+      </Form>
+    </>
+  );
+};
+const params = (param) => {
+  let titles = "";
   switch (param) {
     case "land":
       titles = "Земельные отношения";
@@ -30,6 +73,5 @@ export const OrganizationId = ({ title }) => {
       titles = "Услуги";
       break;
   }
-  axios.get("http://localhost:5000/api/directory/" + param).then((res) => console.log(res.data));
-  return <h1>{titles}</h1>;
+  return titles;
 };
